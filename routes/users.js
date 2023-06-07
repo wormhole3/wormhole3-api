2e8157f83f2ab8af3742c861337c803b84625709
terry3t <wangxi8206@sina.com>
2023-06-08 00:28:03 +0800

    feat:update twitter auth api

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const UserDB = require('../src/db/api/user')
 const { handleError } = require('../src/utils/helper')
+const { ERR_CODE } = require('../config')
 const { get, del, set } = require('../src/db/redis')
 
 /* GET users listing. */
@@ -13,7 +14,11 @@ router.get('/byTwitterId', async (req, res) => {
   const { twitterId } = req.query;
   if (twitterId) {
     const user = await UserDB.getAccountByTwitterId(twitterId);
-    return res.status(200).json(JSON.stringify(user || {}))
+    if (user) {
+      return res.status(200).json(user)
+    } else {
+      return handleError(res, "User not exist", 'User not exist', ERR_CODE.USER_NOT_EXIST);
+    }
   }else {
     return handleError(res, "Invalid Twitter Id", 'Parameter missing, need twitter id', 301)
   }
@@ -24,7 +29,11 @@ router.get('/byUsername', async (req, res) => {
   const { username } = req.query;
   if (username) {
     const user = await UserDB.getAccountByTwitterUsername(username);
-    return res.status(200).json(JSON.stringify(user || {}))
+    if (user) {
+      return res.status(200).json(user)
+    } else {
+      return handleError(res, "User not exist", 'User not exist', ERR_CODE.USER_NOT_EXIST);
+    }
   }else {
     return handleError(res, "Invalid Twitter username", "Parameter missing, need twitter username", 302)
   }
@@ -34,7 +43,11 @@ router.get('/byNearId', async (req, res) => {
   const { nearId } = req.query;
   if (nearId) {
     const user = await UserDB.getAccountByNearId(nearId);
-    return res.status(200).json(JSON.stringify(user || {}))
+    if (user) {
+      return res.status(200).json(user)
+    } else {
+      return handleError(res, "User not exist", 'User not exist', ERR_CODE.USER_NOT_EXIST);
+    }
   }else {
     return handleError(res, "Invalid near id", "Parameter missing, need near id", 302)
   }
